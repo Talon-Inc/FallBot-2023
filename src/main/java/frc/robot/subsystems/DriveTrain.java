1496d3739a4a9500dafb3edd77bb629382c4d23a
madpikachu97 <johnadare3@gmail.com>
2023-10-24 17:52:35 -0500

    complete drivetrain

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,17 +4,45 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
-  TalonSRX leftFrontMotor;
-  TalonSRX leftBackMotor;
-  TalonSRX rightFrontMotor;
-  TalonSRX rightBackMotor;
+  private final WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(1);
+  private final WPI_TalonSRX leftBackmotor = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX rightFrontmotor = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX rightBackmotor = new WPI_TalonSRX(4);
+  private DifferentialDrive Drive = null;
   /** Creates a new DriveTrain. */
   public DriveTrain() {
+    leftFrontMotor.configFactoryDefault();
+    leftBackmotor.configFactoryDefault();
+    rightFrontmotor.configFactoryDefault();
+    rightBackmotor.configFactoryDefault();
+
+  
+    leftBackmotor.follow(leftFrontMotor);
+    rightBackmotor.follow(rightFrontmotor);
+
+   
+    boolean leftInverted = false; 
+    leftFrontMotor.setInverted(leftInverted);
+    rightFrontmotor.setInverted(!leftInverted);
+
+   
+    leftBackmotor.setInverted(InvertType.FollowMaster);
+    rightBackmotor.setInverted(InvertType.FollowMaster);
+    
+   
+    leftFrontMotor.setSensorPhase(true);
+    rightFrontmotor.setSensorPhase(true);
+
+    
+    
+   Drive = new DifferentialDrive(leftFrontMotor, rightFrontmotor);
   
   }
 
@@ -22,10 +50,4 @@ public class DriveTrain extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
-}    config.peakCurrentLimit = 40; 
-config.peakCurrentDuration = 1500; 
-config.continuousCurrentLimit = 30; 
-leftFrontMotor.configAllSettings(config); 
-leftBackMotor.configAllSettings(config);
-rightFrontMotor.configAllSettings(config); /
-rightBackMotor.configAllSettings(config);
+}
